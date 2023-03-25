ebb2176e1d82b09d85e50a6dca5919af2fe72836
Luis Crespo <lcrespom@gmail.com>
2023-03-25 21:14:34 +0100

    Listener and poster work

--- src/lib/Poster.svelte
+++ src/lib/Poster.svelte
@@ -3,6 +3,19 @@
 
 	export let client: MqttClient
 	export let topic: string
+
+	let message = ''
+
+	function postMessage() {
+		client.publish(topic, message, error => {
+			if (error) console.error(error)
+			else console.log('Message posted')
+		})
+	}
 </script>
 
-<div>ToDo: Poster</div>
+<label>
+	Message:
+	<input type="text" bind:value={message} />
+</label>
+<button type="button" on:click={postMessage}>Post</button>
